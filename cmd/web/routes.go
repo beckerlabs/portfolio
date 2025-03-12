a8972ccdb8cb59db8505e7e470b0782efb69f2de
jmbecker <jmbecker2010@gmail.com>
2025-03-11 21:24:21 -0400

    Create about page.

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -4,8 +4,11 @@ import "net/http"
 
 func routes() http.Handler {
 	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
-	mux.HandleFunc("GET /", home)
+	mux.HandleFunc("GET /{$}", home)
+	mux.HandleFunc("GET /about", about)
 
 	return mux
 }
