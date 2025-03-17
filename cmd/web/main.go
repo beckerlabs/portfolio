fda238f19f31f2dfb525f5a64ac54befd69c15e5
jmbecker <jmbecker2010@gmail.com>
2025-03-16 22:22:42 -0400

    Adding in sql connection.
    Add several flags for the address and sql connection.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,13 +1,45 @@
 package main
 
 import (
+	"database/sql"
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsnUser := flag.String("dsnUser", "web", "MySQL data source user")
+	dsnPass := flag.String("dsnPass", "password", "MySQL data source password")
+	flag.Parse()
+
+	dsn := fmt.Sprintf("%s:%s@/beckerlabs?parseTime=true", *dsnUser, *dsnPass)
+	db, err := openDb(dsn)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
 	mux := routes()
 
-	err := http.ListenAndServe(":4000", mux)
+	err = http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
+}
+
+func openDb(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
